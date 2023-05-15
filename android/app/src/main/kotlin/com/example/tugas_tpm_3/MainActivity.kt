ff32b491789c8b0e63f765d472882eb2d5894a6d
alfandi14 <alfandi.xyz@gmail.com>
2023-05-15 22:44:34 +0700

    final commit

---
+++ android/app/src/main/kotlin/com/example/tugas_tpm_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_tpm_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:tugas_tpm_3/view/page_list_film.dart';
+import 'package:tugas_tpm_3/view/page_search_film.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OMDB FILM',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.light().copyWith(primary: Colors.tealAccent),
+      ),
+      home: PageSearchFilm(),
+    );
+  }
+}
